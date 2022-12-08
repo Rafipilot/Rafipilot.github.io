a05d4509b573239280c54e761154d0892de6b0b9
Rafipilot <74052861+Rafipilot@users.noreply.github.com>
2022-12-08 14:13:52 +0000

    4.4

--- particles.js-master/particles.js-master/app.js
+++ particles.js-master/particles.js-master/app.js
@@ -1,4 +1,4 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
+particlesJS.load('particles-js', 'particles.js-master/particles.js-master/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
